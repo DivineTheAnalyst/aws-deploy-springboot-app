9bfd03013f667caf0eeedfd050a44943d806be77
Ijeawele <dnkwocha14@gmail.com>
2025-09-11 02:11:40 +0100

    Add Dockerfile and buildspec for AWS deployment; update Java version in pom.xml and fix package name in TestController

--- src/main/java/com/example/springboot_app/TestController.java
+++ src/main/java/com/example/springboot_app/TestController.java
@@ -1,4 +1,4 @@
-package com.example.springbootawsdeploy;
+package com.example.springboot_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
